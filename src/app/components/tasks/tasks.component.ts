7652a889131bee6fc02c627bba44499927207bf6
Julia Faqir <julia.faqir@pwc.com>
2023-05-15 12:34:29 +0300

    json Server, delete, and reminder toggle functions

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -16,4 +16,13 @@ export class TasksComponent {
     this.taskService.getTasks().subscribe((tasks)=> this.tasks = tasks);
   }
 
+  deleteTask(task: Task) {
+   this.taskService.deleteTask(task).subscribe(()=> (this.tasks = this.tasks.filter(t => t.id !== task.id)))
+  }
+
+  toggleReminder(task: Task) {
+    task.reminder = !task.reminder;
+    console.log(task.reminder);
+  }
+
 }
