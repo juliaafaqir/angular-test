3aa7abeb56f1fcf136aaca41c3dcd339d47b9c7e
Julia Faqir <julia.faqir@pwc.com>
2023-05-17 13:25:43 +0300

    Header & Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,17 @@ import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
+import { SiteHeaderComponent } from './components/site-header/site-header.component';
+import { FooterComponent } from './components/footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ButtonComponent,
     TasksComponent,
-    TaskItemComponent
+    TaskItemComponent,
+    SiteHeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
